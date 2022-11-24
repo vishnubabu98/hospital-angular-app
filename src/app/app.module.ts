093e4e5df83a437bfd26bbcf2c61f2b648861c0a
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-24 12:02:42 +0530

    add patient delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 import { PatientSearchComponent } from './patient-search/patient-search.component';
+import { PatientDeleteComponent } from './patient-delete/patient-delete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PatientEntryComponent,
-    PatientSearchComponent
+    PatientSearchComponent,
+    PatientDeleteComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/patient-delete/patient-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-delete',
+  templateUrl: './patient-delete.component.html',
+  styleUrls: ['./patient-delete.component.css']
+})
+export class PatientDeleteComponent {
+
+}
