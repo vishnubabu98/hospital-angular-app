31dadcb5f712e983d6b54007f026f3d2b3d9327b
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-24 11:03:06 +0530

    add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientEntryComponent } from './patient-entry/patient-entry.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientEntryComponent
   ],
   imports: [
     BrowserModule,
