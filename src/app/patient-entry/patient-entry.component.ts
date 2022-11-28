2f17468dba4e8211cfdf22858169d13d1ed9ef7d
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-11-28 21:30:49 +0530

    add values

--- src/app/patient-entry/patient-entry.component.ts
+++ src/app/patient-entry/patient-entry.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./patient-entry.component.css']
 })
 export class PatientEntryComponent {
+  patientname=""
+  patientid=""
+  Address=""
+  contact=""
+  dateofappo=""
+  image=""
+  docname=""
+
+  readValues=()=>
+  {
+    let data:any=
+    {
+      "patientname":this.patientname,
+      "patientid":this.patientid,
+      "Address":this.Address,
+      "contact":this.contact,
+      "dateofappo":this.dateofappo,
+      "image":this.image,
+      "docname":this.docname
+    }
+    console.log(data)
+  }
+
+
 
 }
